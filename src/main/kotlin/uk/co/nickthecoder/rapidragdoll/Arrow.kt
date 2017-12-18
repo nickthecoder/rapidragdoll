f84b5a8c4acedd5f27774f50295595805739d9f0
Nick Robinson <nickthecoder@gmail.com>
2017-12-18 08:12:38 +0000

    Draw an arrow from the laucher to the mouse position (but limit the length to the launcher's maximum speed).

---
+++ src/main/kotlin/uk/co/nickthecoder/rapidragdoll/Arrow.kt
@@ -0,0 +1,32 @@
+package uk.co.nickthecoder.rapidragdoll
+
+import org.joml.Vector2d
+import uk.co.nickthecoder.tickle.AbstractRole
+
+class Arrow : AbstractRole() {
+
+    private val mouse = Vector2d()
+    private val tempVector = Vector2d()
+
+    override fun tick() {
+        actor.stage?.firstView()?.mousePosition(mouse)
+        Play.instance.launcher?.let {
+            actor.position.set(it.actor.position)
+            mouse.sub(actor.position, tempVector)
+            val magnitude = tempVector.length()
+
+            if (magnitude > it.speed) {
+                // Show the Aim actor, and draw the arrow from the launcher towards the mouse position
+                // With magnitude of the maximum speed of the launcher.
+                Play.instance.aim?.actor?.event("default")
+                tempVector.normalize(it.speed).add(actor.position)
+                actor.ninePatchAppearance?.lineTo(tempVector)
+            } else {
+                // Hide the Aim actor, and draw the arrow from the launcher to the mouse position
+                Play.instance.aim?.actor?.hide()
+                actor.ninePatchAppearance?.lineTo(mouse)
+            }
+        }
+    }
+
+}
